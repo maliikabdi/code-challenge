42bcf722d528c7fc0abc91da2c954f4f3462f880
maliikabdi <lildarwish13158@gmail.com>
2024-10-07 15:14:36 +0300

    completed lab

--- 1.js
+++ 1.js
@@ -5,24 +5,25 @@
 //D = 40 TO 49,
 //E = LESS 40.
 
-let mark = 50
-if (mark>79 && mark<=100){
-    console.log("A")
+let mark = 35; // Initialize the 'mark' variable with a value of 35
+
+// Check if the mark is greater than 79 and less than or equal to 100, assign grade 'A'
+if (mark > 79 && mark <= 100) {
+    console.log("A");
 }
-else if(mark>=60 && mark<=79){
-    console.log("B")
-
+// Check if the mark is between 60 and 79, inclusive, assign grade 'B'
+else if (mark >= 60 && mark <= 79) {
+    console.log("B");
 }
-else if(mark>=49 && mark<=59){
-    console.log("C")
+// Check if the mark is between 50 and 59, inclusive, assign grade 'C'
+else if (mark >= 50 && mark <= 59) {
+    console.log("C");
 }
-
-
-else if(mark>=49 && mark<=49){
-    console.log("D")
+// Check if the mark is between 40 and 49, inclusive, assign grade 'D'
+else if (mark >= 40 && mark <= 49) {
+    console.log("D");
 }
-
-
-else if(mark<=40 && mark>=0){
-    console.log("E")
+// Check if the mark is below 40 but not negative, assign grade 'E'
+else if (mark < 40 && mark >= 0) {
+    console.log("E");
 }
--- 2.js
+++ 2.js
@@ -3,23 +3,23 @@ function speedDetector(speed) {
     const kmPerDemeritPoint = 5;
     const maxDemeritPoint = 12;
 
-// if speed is less than or equal to the speed limit
-if (speed <= speedLimit){
-    console.log("OK");
+    // if speed is less than or equal to the speed limit
+    if (speed >= speedLimit) {
+        console.log("OK");
+    }
+    //calculate demerit points and excessspeed
+    let excessSpeed = speed - speedLimit;
+    let demeritPoints = excessSpeed / kmPerDemeritPoint;
+    //if the excess speed is not excatly divisible by 5, floor the points
+    if (excessSpeed % kmPerDemeritPoint !== 0) {
+        demeritPoints = Math.floor(demeritPoints);
+    }
+    //print out the total demerits points.
+    console.log("points:", demeritPoints);
+    //if the demerits points are exceeded by 12, suspend the license
+    if (demeritPoints > maxDemeritPoint) {
+        console.log("License suspended");
+    }
 }
-//calculate demerit points and excessspeed
-let excessSpeed = speed - speedLimit;
-let demeritPoints = excessSpeed / kmPerDemeritPoint;
-//if the excess speed is not excatly divisible by 5, floor the points
-if(excessSpeed % kmPerDemeritPoint !==0){
-    demeritPoints = math.floor(demeritPoint);
-}
-//print out the total demerits points.
-console.log("points:", demeritPoints);
-//if the demerits points are exceeded by 12, suspend the license
-if (demeritPoints > maxDemeritPoint){
-    console.log("License suspended");
-}
-}
-let speed = 765;
+let speed = "89";
 speedDetector(speed);
